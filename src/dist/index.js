c6f80949ce8868542251d9c8146d4b17029b56fe
Cristiano Colacillo <ccolacillo@infinitaslearning.com>
2025-03-18 11:43:53 +0100

    chore: project set up

---
+++ src/dist/index.js
@@ -0,0 +1,4 @@
+#! /usr/bin/env node
+"use strict";
+// ^ That “shebang” line makes it runnable as an executable if you chmod +x it.
+console.log("Hello from Task Planner CLI!");
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+#! /usr/bin/env node
+// ^ That “shebang” line makes it runnable as an executable if you chmod +x it.
+console.log("Hello from Task Planner CLI!");
